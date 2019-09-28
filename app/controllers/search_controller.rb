998c44a5f9a24ac2f0c2054a5924c7e029c642a6
Joao Torres <joao.torres@sequra.es>
2019-09-28 15:22:21 +0200

    Improve results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,19 @@ class SearchController < ApplicationController
     'ship' => 'ship',
     'plane' => 'plane'
   }
+
+  TYPE_COLOR = {
+    'Chill' => '#4BA97A',
+    'Historical' => '#D66C34',
+    'Adventure' => '#D6347A',
+    'Romantic' => '#B134D6',
+    'Foodie' => '#F9971E',
+    'Party' => '#E30707',
+  }
+
   def index
     @results = Search.run!(params.permit!.to_h)
   end
+
+
 end
--- app/mutations/search.rb
+++ app/mutations/search.rb
@@ -4,7 +4,7 @@ class Search < Mutations::Command
   optional do
     float :latitude, empty: true, nils: true
     float :longitude, empty: true, nils: true
-    integer :budget, empty: true, nils: true
+    integer :budget, empty: true, nils: true, default: 100
     date :start_date, empty: true, nils: true
     date :end_date, empty: true, nils: true
     # string :type, empty: true, nils: true
@@ -15,13 +15,13 @@ class Search < Mutations::Command
     results = HTTParty.get("https://kiwicom-prod.apigee.net/locations/radius?apikey=#{ENV["KIWI_API_KEY"]}&term=BCN&radius=250&locale=en-US&location_types=airport&limit=20&active_only=true")
     results["locations"].drop(1).map do |l|
       {
-        place: l["name"],
-        type: 'chill',
+        place: l["name"].split.first.split("–").first,
+        type: type,
         img: img_url(l["name"]),
         options: [
-          { type: 'bus', price: price, co2: 10 },
-          { type: 'train', price: price, co2: 10 },
-          { type: 'plane', price: price, co2: 10 }
+          { type: 'bus', price: price, co2: co2 },
+          { type: 'train', price: price, co2: co2 },
+          { type: 'plane', price: price, co2: co2 }
         ]
       }
     end
@@ -89,5 +89,13 @@ class Search < Mutations::Command
     {
       'result' => location
     }
+  end
+
+  def type
+    %w(Chill Historical Adventure Romantic Foodie Party).sample
+  end
+
+  def co2
+    10 + rand(10)
   end
 end
